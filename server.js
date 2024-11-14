721f1c5dce8b1d63cd3c09277c80e9a14c76a67f
ptthaothao <graciept2109@gmail.com>
2024-11-14 16:38:26 +0700

    upgrade

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
-const express = require('express');
-const { PORT } = require('./config.js');
+const express = require("express");
+var cors = require("cors");
+var morgan = require("morgan");
+const { PORT } = require("./config.js");
 
 let app = express();
-app.use(express.static('wwwroot'));
-app.use(require('./routes/auth.js'));
-app.use(require('./routes/models.js'));
-app.listen(PORT, function () { console.log(`Server listening on port ${PORT}...`); });
+// app.use(express.static("wwwroot"));
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+    methods: ["GET", "POST", "PATCH", "PUT", "DELETE"],
+    preflightContinue: true
+  })
+);
+app.use(express.json());
+app.use(morgan("tiny"));
+
+app.use(require("./routes/auth.js"));
+app.use(require("./routes/models.js"));
+app.listen(PORT, function () {
+  console.log(`Server listening on port ${PORT}...`);
+});
